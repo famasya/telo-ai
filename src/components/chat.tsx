ba6194b7a1cdc57d09414c5a94a86a6567f20343
Abid Famasya <abid.famasya@gmail.com>
2025-12-14 18:54:33 +0700

    feat: initial rag experiment

---
+++ src/components/chat.tsx
@@ -0,0 +1,59 @@
+import { useChat } from "@ai-sdk/react";
+import { useState } from "react";
+
+export default function Chat() {
+  const [input, setInput] = useState("");
+  const { messages, sendMessage } = useChat();
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      <div className="space-y-4">
+        {messages.map(m => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            <div>
+              <div className="font-bold">{m.role}</div>
+              {m.parts.map((part, index) => {
+                switch (part.type) {
+                  case 'text':
+                    return <p key={index.toString()}>{part.text}</p>;
+                  case 'tool-documentSearch':
+                    return (
+                      <p key={index.toString()}>
+                        call{part.state === 'output-available' ? 'ed' : 'ing'}{' '}
+                        tool: {part.type}
+                        <pre className="my-4 bg-zinc-100 p-2 rounded-sm">
+                          {JSON.stringify(part.input, null, 2)}
+                        </pre>
+                        {part.state === 'output-available' && part.output && (
+                          <pre className="my-4 bg-green-100 p-2 rounded-sm">
+                            {JSON.stringify(part.output, null, 2)}
+                          </pre>
+                        )}
+                      </p>
+                    );
+
+                  default:
+                    return null;
+                }
+              })}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          sendMessage({ text: input });
+          setInput('');
+        }}
+      >
+        <input
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={e => setInput(e.currentTarget.value)}
+        />
+      </form>
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,15 @@
-import { Button } from "@/components/ui/button";
+import Chat from "@/components/chat";
 import { createFileRoute } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-import { env } from "cloudflare:workers";
 
 export const Route = createFileRoute("/")({
-	loader: () => getData(),
 	component: Home,
 });
 
-const getData = createServerFn().handler(() => {
-	return {
-		message: `Running in ${navigator.userAgent}`,
-		myVar: env.MY_VAR,
-	};
-});
-
 function Home() {
-	const data = Route.useLoaderData();
 
 	return (
 		<div className="flex flex-col gap-2 h-screen w-full items-center justify-center">
-			<Button>Click Me</Button>
-			<p>{data.message}</p>
-			<p>{data.myVar}</p>
+			<Chat />
 		</div>
 	);
 }
